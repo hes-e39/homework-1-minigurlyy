7c83f8636f9ae93a04b7d0438f8b6187edfbaae9
minigurlyy <srzahid99@gmail.com>
2024-09-16 17:49:49 -0700

    fixed the comp colors issue. They are in an array.

--- index.js
+++ index.js
@@ -10,8 +10,50 @@ const COLORS = 'https://nt-cdn.s3.amazonaws.com/colors.json';
  * @param compHex filter for complementary color hex code
  * @returns Promise
  */
-const fetchColors = ({ name, hex, compName, compHex }) => {
-  throw Error('Not implemented');
+
+// retrieving colors data using fetch:
+const fetchColors = async ({ name, hex, compName, compHex }) => {
+  // fetch data from API:
+  const response = await fetch(COLORS);
+
+  if(!response.ok) {
+    throw Error('Error: Looks like something went wrong. Failed to fetch colors.');
+  }
+
+  const colors = await response.json();
+
+  let filteredColors = colors;
+
+  // filter by name
+  if(name) {
+    filteredColors = filteredColors.filter(function (color) {
+      return color.name.toLowerCase().includes(name.toLowerCase());
+    });
+  }
+
+  // filter by HEX CODE (WITHOUT #)
+  if(hex) {
+    filteredColors = filteredColors.filter(function (color) {
+      return color.hex.toLowerCase() === hex.toLowerCase();
+    });
+  }
+
+  // filter by complementary color (ignore case)
+if(compName) {
+  filteredColors = filteredColors.filter(function (color) {
+    return color.complementaryColor && color.complementaryColor.name.toLowerCase().includes(compName.toLowerCase());
+  });
+}
+
+  // filter by complementary color hex code (WITHOUT #)
+  if(compHex) {
+    filteredColors = filteredColors.filter(function (color) {
+      return color.complementaryColor && color.complementaryColor.hex.toLowerCase() === compHex.toLowerCase();
+    });
+  }
+
+  // return filteresed colors:
+  return filteredColors;
 };
 
 // Leave this here
